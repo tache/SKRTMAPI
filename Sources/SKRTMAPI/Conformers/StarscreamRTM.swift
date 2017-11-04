3a9668a4dd7a5744ea8700b593d093fc7b65ef18
tache <chris@mondocoolstudios.com>
2017-11-04 13:00:54 -0400

    Updates for Swift 4

--- Sources/SKRTMAPI/Conformers/StarscreamRTM.swift
+++ Sources/SKRTMAPI/Conformers/StarscreamRTM.swift
@@ -26,7 +26,8 @@ import Foundation
 import SKCore
 import Starscream
 
-public class StarscreamRTM: RTMWebSocket, WebSocketDelegate {
+    public class StarscreamRTM: RTMWebSocket, WebSocketDelegate {
+
     public weak var delegate: RTMDelegate?
     private var webSocket: WebSocket?
 
@@ -55,19 +56,19 @@ public class StarscreamRTM: RTMWebSocket, WebSocketDelegate {
     }
 
     // MARK: - WebSocketDelegate
-    public func websocketDidConnect(socket: WebSocket) {
+    public func websocketDidConnect(socket: WebSocketClient) {
         delegate?.didConnect()
     }
 
-    public func websocketDidDisconnect(socket: WebSocket, error: NSError?) {
+    public func websocketDidDisconnect(socket: WebSocketClient, error: Error?) {
         webSocket = nil
         delegate?.disconnected()
     }
 
-    public func websocketDidReceiveMessage(socket: WebSocket, text: String) {
+    public func websocketDidReceiveMessage(socket: WebSocketClient, text: String) {
         delegate?.receivedMessage(text)
     }
 
-    public func websocketDidReceiveData(socket: WebSocket, data: Data) {}
+    public func websocketDidReceiveData(socket: WebSocketClient, data: Data) {}
 }
 #endif
